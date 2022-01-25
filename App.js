7fa8cb750fa45b0dac87a0db488c85d56ccbb93f
shriram-byjus <shriram.s@byjus.com>
2022-01-25 20:33:43 +0530

    Display Alert with 3 buttons along with text

--- App.js
+++ App.js
@@ -4,13 +4,33 @@ import { StyleSheet, StatusBar, Button, Text, View, Alert } from 'react-native';
 type Props = {};
 export default class App extends Component <Props> {
 
+    displayAlert = () =>
+    Alert.alert(
+            "Project Name:",
+            "Byjus - Mentor Connect",
+            [
+              {
+                text: "Ask me later",
+                onPress: () => console.log("Ask me later pressed")
+              },
+              {
+                text: "Cancel",
+                onPress: () => console.log('cancel button pressed')                
+              },
+              {
+                text: "OK",
+                onPress: () => console.log("OK Pressed")
+              }
+            ]
+    );
+
     render() {
     return (
         <View>
           <Text style={styles.welcome}> Hello Shriram !!</Text> 
           <Button 
-                 title="Display Project Name"
-                 onPress={() => Alert.alert('Byjus Mentor Connect')}
+                 title="Display Project Name"                
+                 onPress={this.displayAlert}
           > </Button>   
           <StatusBar backgroundColor="#b3e6ff"  barStyle = "dark-content"></StatusBar>      
         </View>
@@ -28,4 +48,3 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',   
   }
 });
-
